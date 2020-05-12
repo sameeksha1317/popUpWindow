a21c25d9b5fe9f2aca4c33d6f5f7e840071ef607
sameeksha <itsmesameeksha1317@gmail.com>
2020-05-13 02:46:55 +0530

    improved

--- popUpWindow.js
+++ popUpWindow.js
@@ -1,20 +1,22 @@
-// var parent=document.querySelector(".paraParent"),
-//         bttn=document.querySelector("button"),
-// h=document.querySelector(".x");
-//
-// bttn.addEventListener("click",appear);
-// function appear(){
-// parent.style.display="block";
-// content.style.filter="blur(10px)";
-// }
-// h.addEventListener("click",disappear);
-// function disappear(){
-// parent.style.display="none";
-// content.style.filter="blur(0px)";
-// }
+
 var parent=document.querySelector(".paraParent"),
-buttn=document.querySelector(".btn");
+buttn=document.querySelector("button");
 buttn.addEventListener("click",appear);
+X =document.querySelector(".x");
+section =document.querySelector(".content");
 function appear(){
 parent.style.display="block";
+section.style.filter="blur(6px)";
+}
+X.addEventListener("click",disappear);
+function disappear(){
+parent.style.display="none";
+section.style.filter ="blur(0px)";
+}
+parent.addEventListener("click",disappearparent);
+function disappearparent(e){
+if(e.target.className =="paraParent"){
+  parent.style.display="none";
+  section.style.filter ="blur(0px)";
+}
 }
