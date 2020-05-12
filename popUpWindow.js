7aa31f82463d0847123f48ba31739c27ef58148c
sameeksha <itsmesameeksha1317@gmail.com>
2020-05-12 17:42:27 +0530

    first

---
+++ popUpWindow.js
@@ -0,0 +1,20 @@
+// var parent=document.querySelector(".paraParent"),
+//         bttn=document.querySelector("button"),
+// h=document.querySelector(".x");
+//
+// bttn.addEventListener("click",appear);
+// function appear(){
+// parent.style.display="block";
+// content.style.filter="blur(10px)";
+// }
+// h.addEventListener("click",disappear);
+// function disappear(){
+// parent.style.display="none";
+// content.style.filter="blur(0px)";
+// }
+var parent=document.querySelector(".paraParent"),
+buttn=document.querySelector(".btn");
+buttn.addEventListener("click",appear);
+function appear(){
+parent.style.display="block";
+}
